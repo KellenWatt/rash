06e2c650910e0f81913bdf7d54f9d834c592806a
Kellen Watt <kbw6d9@mst.edu>
2020-08-23 19:59:29 -0500

    Baseline

--- rash.rb
+++ rash.rb
@@ -1,7 +1,17 @@
-class Environment 
+class Environment
+
+  attr_reader :aliases
+  attr_accessor :prompt
+
   def initialize
     @working_directory = Dir.home
     @aliases = Hash.new
+    @default_permissions
+    @prompt = {
+      # Make this optionally a lambda or string
+      # This works for affecting the string
+      # :PROMPT_I => "%N(%m):%03n:%i %~> ".tap {|s| def s.dup; gsub('%~', Dir.pwd); end },
+    }
   end
 
   def chdir(dir)
@@ -12,15 +22,13 @@ class Environment
   def method_missing(m, *args, &block) 
     if args.length == 0 
       self.define_singleton_method(m) do
-        ENV[__method__.to_s]
+        ENV[__method__.to_s.upcase]
       end
       self.send(m)
     else
       super
     end
   end
-
-  attr_reader :aliases
 
   def make_alias(new_func, old_func)
     @aliases[new_func.to_sym] = old_func.to_sym
@@ -30,29 +38,46 @@ class Environment
     @aliases.delete(func.to_sym)
   end
 
+  # recursive aliases not currently possible. In the works
   def resolve_alias(f)
     al = f.to_sym
     if @aliases.include?(al)
       @aliases[al]
     else
-      al
+      f
     end
   end
+  
+  private
 
+  class Directory
+    def initialize(path)
+      @path = path
+    end
+
+    def add_local_method(name, &block)
+      self.define_method(name, &block)
+    end
+
+    def to_s
+      @path
+    end
+  end
 end
 
 $env = Environment.new
 
-# Simple aliases for ruby/irb builtins
-alias logout exit
-
-
-def cd(dir=nil, *junk)
+# note for later documentation: any aliases of cd must be functions, not 
+# environmental aliases. Limitation of implementation.
+def cd(dir=nil, *_junk)
+  old = Dir.pwd
   if dir.nil? 
     $env.chdir(Dir.home)
   else
     $env.chdir(dir)
   end
+  ENV["OLDPWD"] = old
+  Dir.pwd
 end
 
 def run(filename)
@@ -63,20 +88,41 @@ def run(filename)
   `#{exe}`
 end
 
+# Defines `bash` psuedo-compatibility. Filesystem effects happen like normal 
+# and environmentl changes are copied
 def sourcesh(file) 
-  bash_env = lambda do |cmd=nil|
+  bash_env = lambda do |cmd = nil|
     tmpenv = `#{cmd + ';' if cmd} printenv`
     tmpenv.split("\n").grep(/[a-zA-z0-9_]+=.*/).map {|l| l.split("=")}
   end
   bash_source = lambda do |f|
-    Hash[bash_env.("source #{File.realpath f}") - bash_env.()]
+    Hash[bash_env.call("source #{File.realpath f}") - bash_env.()]
   end
 
-  bash_source.(file).each {|k,v| ENV[k] = v if k != "SHLVL" && k != "_"}
+  bash_source.call(file).each {|k,v| ENV[k] = v if k != "SHLVL" && k != "_"}
 end
 
+# Note that I defy convention and don't define `respond_to_missing?`. This
+# is because doing so as-is would involve running the command itself, which 
+# would be 1) probably very slow, and 2) potentially dangerous if the command 
+# has side effects.
 def self.method_missing(m, *args, &block) 
   puts m
   super if system("#{$env.resolve_alias(m)} #{args.join(" ")}").nil?
 end
+
+
+IRB.conf[:PROMPT][:RASH] = {
+  :PROMPT_I => "rash   ",
+  :PROMPT_N => "rash-n ",
+  :PROMPT_S => "rash-s ",
+  :PROMPT_C => "rash-c ",
+  :RETURN => "%s\n" # used to printf
+}
+IRB.conf[:PROMPT_MODE] = :RASH
+IRB.conf[:SAVE_HISTORY] = 1000
+IRB.conf[:AP_NAME] = "rash"
+
+run_command_file = "#{$env.HOME}/.rashrc"
+require_relative run_command_file if File.file?(run_command_file)
 
