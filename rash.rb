8e7134b8584ac4e705be4dda44dd0bc43e40da35
Kellen Watt <kbw6d9@mst.edu>
2020-08-25 01:12:29 -0500

    Add compound IO modifiers

--- rash.rb
+++ rash.rb
@@ -206,13 +206,47 @@ def with_stderr_as(file = STDERR)
 end
 
 def with_stdin_as(file = STDIN)
-# needs block
   $env.stdin = file
   if block_given?
     begin
       yield
     ensure
       $env.reset_stdin
+    end
+  end
+end
+
+def with_stdout_as_stderr
+  $env.stdout = $stderr
+  if block_given?
+    begin
+      yield
+    ensure
+      $env.reset_stdout
+    end
+  end
+end
+
+def with_stderr_as_stdout
+  $env.stderr = $stdout
+  if block_given?
+    begin
+      yield
+    ensure
+      $env.reset_stderr
+    end
+  end
+end
+
+def with_all_out_as(file)
+  $env.stdout = file
+  $env.stderr = $stdout
+  if block_given?
+    begin
+      yield
+    ensure
+      $env.reset_stdout
+      $env.reset_stderr
     end
   end
 end
