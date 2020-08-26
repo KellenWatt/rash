8a5af09e9cf70c68a05ed2236db11b0e8c2f7282
Kellen Watt <kbw6d9@mst.edu>
2020-08-26 15:43:25 -0500

    Add job control methods to Environment

---
+++ lib/jobcontrol.rb
@@ -0,0 +1,17 @@
+class Environment
+  def jobs
+    @active_jobs.keep_if {|pid| Process.kill(0, pid) rescue false}
+  end
+
+  def async(&block)
+    pid = fork {
+      block.call
+      exit!(true)
+    }
+    @active_jobs << pid
+    Process.detach(pid)
+    pid
+  end
+end
+  
+
