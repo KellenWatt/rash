56d69f53f2d4acb8339ca33ac33b9a1804dd1898
Kellen Watt <kbw6d9@mst.edu>
2020-09-09 22:43:26 -0500

    Prohibit capturing from interacting with pipelining

--- lib/rash/capturing.rb
+++ lib/rash/capturing.rb
@@ -2,11 +2,14 @@ class Environment
   def capture_block(&block)
     raise ArgumentError.new("no block provided") unless block_given?
     result = nil
+    old_pipeline = @in_pipeline
     begin
       reader, writer = IO.pipe
       self.stdout = writer
+      @in_pipeline = false
       block.call
     ensure
+      @in_pipeline = old_pipeline
       reset_stdout
       writer.close
       result = reader.read
